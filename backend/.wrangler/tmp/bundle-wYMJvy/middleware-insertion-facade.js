d4b7729016117c14484f8bd136bce8a0d2b0935e
hlink <linh123kt18@gmail.com>
2025-05-19 00:22:48 +0700

    update dockerfile

---
+++ backend/.wrangler/tmp/bundle-wYMJvy/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "D:\\PTUDDN - Copy\\PenCraft-Full-Stack-Blogging-Application-main\\PenCraft-Full-Stack-Blogging-Application-main\\backend\\src\\index.ts";
+				import * as __MIDDLEWARE_0__ from "D:\\PTUDDN - Copy\\PenCraft-Full-Stack-Blogging-Application-main\\PenCraft-Full-Stack-Blogging-Application-main\\backend\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "D:\\PTUDDN - Copy\\PenCraft-Full-Stack-Blogging-Application-main\\PenCraft-Full-Stack-Blogging-Application-main\\backend\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "D:\\PTUDDN - Copy\\PenCraft-Full-Stack-Blogging-Application-main\\PenCraft-Full-Stack-Blogging-Application-main\\backend\\src\\index.ts";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
